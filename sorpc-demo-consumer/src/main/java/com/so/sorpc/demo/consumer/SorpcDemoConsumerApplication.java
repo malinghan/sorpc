00cf99c9ec2207632a18d34c0e470544f7049259
malinghan <linghan.ma@gmail.com>
2024-03-31 19:43:39 +0800

    submit

---
+++ sorpc-demo-consumer/src/main/java/com/so/sorpc/demo/consumer/SorpcDemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.so.sorpc.demo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorpcDemoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorpcDemoConsumerApplication.class, args);
+    }
+
+}
---
+++ sorpc-demo-consumer/src/test/java/com/so/sorpc/demo/consumer/SorpcDemoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.so.sorpc.demo.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorpcDemoConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
